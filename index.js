2edbb43be627562f0a29451acb4ab6b4b1c4a503
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-12-03 16:43:36 +0100

    fix: update help, rename methods

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Authenticator = require('./src/authenticator');
 const Logger = require('./src/logger').Logger;
 const PasswordManager = require('./src/password-manager');
-const {CLIManager, OPTION_DEFINITIONS, USAGE_SECTIONS} = require('./src/cli-manager');
+const {CLIManager, OPTION_DEFINITIONS, generateUsageSections} = require('./src/cli-manager');
 
 let cliManager = new CLIManager();
 let options = cliManager.parse(OPTION_DEFINITIONS);
 
 if (options.help || process.argv.length == 2) {
-  let help = cliManager.getHelp(USAGE_SECTIONS);
+  let usageSections = generateUsageSections(OPTION_DEFINITIONS);
+  let help = cliManager.getHelp(usageSections);
   console.log(help);
   process.exit(1);
 }
@@ -16,14 +17,13 @@ if (options.help || process.argv.length == 2) {
 let logger = new Logger(options.log);
 let pm = new PasswordManager(logger, {
   passwordFile: options['pass-file'],
+  passwordPrefix: options['pass-prefix'],
   bufferSize: options['max-requests']
 });
-pm.prepareBuffer();
+pm.fillBuffer();
 
 let authenticator = new Authenticator(pm, logger, {
   url: options.url,
   user: options.user,
   maxRequests: options['max-requests']
 });
-// 4YIbe15P
-authenticator.makeSingleRequest('4YIbe1');
