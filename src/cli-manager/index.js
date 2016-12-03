6d94e07bf15c03e2a0419b66cf9758d1025940a7
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-12-03 16:04:57 +0100

    fix(cli-manager): naming

--- src/cli-manager/index.js
+++ src/cli-manager/index.js
@@ -1,9 +1,9 @@
-const cliManager = require('./cli-manager');
+const CLIManager = require('./cli-manager');
 const OPTION_DEFINITIONS = require('./option-definitions');
 const USAGE_SECTIONS = require('./usage-sections');
 
 module.exports = {
-  cliManager,
+  CLIManager,
   OPTION_DEFINITIONS,
   USAGE_SECTIONS
 };
