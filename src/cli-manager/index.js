566bc52273ab01fb17cfbbe5321b79976eb78a75
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-12-03 16:40:59 +0100

    feat(cli-manager): add help

--- src/cli-manager/index.js
+++ src/cli-manager/index.js
@@ -1,9 +1,9 @@
 const CLIManager = require('./cli-manager');
 const OPTION_DEFINITIONS = require('./option-definitions');
-const USAGE_SECTIONS = require('./usage-sections');
+const generateUsageSections = require('./generate-usage-sections');
 
 module.exports = {
   CLIManager,
   OPTION_DEFINITIONS,
-  USAGE_SECTIONS
+  generateUsageSections
 };
--- src/cli-manager/option-definitions.js
+++ src/cli-manager/option-definitions.js
@@ -4,7 +4,7 @@ const DEFAULT_OPTIONS = {
   url: 'http://mini.pw.edu.pl/~brodka/PZ/zad_A/zadanie_a.zip',
   user: 's3-pz',
   maxRequests: 100,
-  passPrefix: ''
+  passPrefix: '4YIbe15P'
 };
 
 const OPTION_DEFINITIONS = [
@@ -12,8 +12,8 @@ const OPTION_DEFINITIONS = [
   { name: 'log', alias: 'l', type: Number, defaultValue: LOG_LEVELS.REGULAR, description: 'Set logging level (1 - silent, 2 - error, 3 - warn, 4 - regular, 5 - verbose)' },
   { name: 'url', type: String, defaultValue: DEFAULT_OPTIONS.url, description: 'URL to a resource that required authorization' },
   { name: 'user', type: String, defaultValue: DEFAULT_OPTIONS.user, description: 'Username to try to authorize with' },
-  { name: 'pass-file', type: String, description: 'Path to a file with passwords' },
-  { name: 'pass-prefix', type: String, defaultValue: DEFAULT_OPTIONS.passPrefix, description: 'Password prefix' },
+  { name: 'pass-file', alias: 'f', type: String, description: 'Path to a file with passwords' },
+  { name: 'pass-prefix', alias: 'p', type: String, defaultValue: DEFAULT_OPTIONS.passPrefix, description: 'Password prefix' },
   { name: 'max-requests', type: Number, defaultValue: DEFAULT_OPTIONS.maxRequests, description: 'Upper limit for concurrent requests amount' },
   { name: 'help', alias: 'h', type: Boolean, description: 'Print this usage guide' }
 ];
